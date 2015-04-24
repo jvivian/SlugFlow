2e68041fa2cca07576b98c005c6a83983ef80f28
John Vivian <jtvivian@gmail.com>
2015-04-24 14:47:31 -0700

    Pep8 Tests

--- Dag2Tree/test_Dag2Tree.py
+++ Dag2Tree/test_Dag2Tree.py
@@ -6,20 +6,18 @@ class Dag2Tree(unittest.TestCase):
 
     def test_isDag(self):
         G = nx.DiGraph()
-        G.add_edges_from([(1,1)])
+        G.add_edges_from([(1, 1)])
         self.assertRaises(AssertionError, convert, G)
 
     def test_isNxObject(self):
-        G='potato'
+        G = 'potato'
         self.assertRaises(AttributeError, convert, G)
 
     def test_hasOneSourceNode(self):
         G = nx.DiGraph()
-        G.add_edges_from([(2,4),(3,4),(3,5)])
+        G.add_edges_from([(2, 4), (3, 4), (3, 5)])
         G = convert(G)
-        self.assertEqual(1, len([node for node in G.nodes() if not nx.ancestors(G,node)]))
-
-
+        self.assertEqual(1, len([node for node in G.nodes() if not nx.ancestors(G, node)]))
 
 
 def main():
