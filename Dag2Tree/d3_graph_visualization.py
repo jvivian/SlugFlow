ee8004922e2ae7f650553d1b6f371a32751c10e3
John Vivian <jtvivian@gmail.com>
2015-05-01 14:03:25 -0700

    Attempts to illustrate D3 force

--- Dag2Tree/d3_graph_visualization.py
+++ Dag2Tree/d3_graph_visualization.py
@@ -21,6 +21,21 @@ G = Dag2Tree.convert(G)
 # so add a name to each node
 for n in G:
     G.node[n]['name'] = n
+
+red_edges =  [edge for edge in G.edges() if G.get_edge_data(edge[0],edge[1])['type'] == 'follow-on']
+edge_colours = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
+black_edges = [edge for edge in G.edges() if edge not in red_edges]
+
+pseudonodes = {node:node for node in G.nodes() if 'pseudo' in G.node[node]}
+node_labels = {node:node for node in G.nodes() if node not in pseudonodes}
+
+pos = nx.circular_layout(G)
+nx.draw_networkx_labels(G, pos, labels=node_labels)
+nx.draw_networkx_nodes(G, pos, nodelist=node_labels, node_color='r')
+nx.draw_networkx_nodes(G, pos, nodelist=pseudonodes, node_color='k')
+nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
+nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=True)
+
 # write json formatted data
 d = json_graph.node_link_data(G) # node-link format to serialize
 # write json
